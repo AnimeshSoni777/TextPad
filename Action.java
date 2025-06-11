9f729eb0f72447386f579be07f3529ae2a309440
AnimeshSoni777 <157093358+AnimeshSoni777@users.noreply.github.com>
2025-06-11 21:02:31 +0530

    Action class is for edit operations.

---
+++ Action.java
@@ -0,0 +1,11 @@
+public class Action {
+    String type; 
+    int position;
+    char character;
+
+    public Action(String type, int position, char character) {
+        this.type = type;
+        this.position = position;
+        this.character = character;
+    }
+}
